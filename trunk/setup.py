b681b8af683489283e4da08bd287177be63c6109
jpivarski <jpivarski@3e5966db-ab3a-0410-9ec6-733648f28d8f>
2008-06-21 08:28:03 +0000

    added args for quick parameter arguments in order, Simplex access, and a --with-minuit= switch to make installation easier

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+import sys, getopt
+
+minuitdir = "../Minuit-1_7_9"
+otherargs = []
+for arg in sys.argv:
+  if arg.find("with-minuit") != -1:
+    optlist, argv = getopt.getopt([arg], "", ["with-minuit="])
+    for name, value in optlist:
+      if name == "--with-minuit":
+        minuitdir = value
+  else:
+    otherargs.append(arg)
+sys.argv = otherargs
+
+print "************************************************************************************************"
+print "Assuming Minuit directory is: %s" % minuitdir
+print "************************************************************************************************"
+
 from distutils.core import setup, Extension
 import os, subprocess
 
@@ -14,8 +32,7 @@ setup(name="pyMinuit",
       package_dir={"": "lib"},
       ext_modules=[Extension(os.path.join("minuit"),
                              [os.path.join("minuit.cpp")],
-                             library_dirs=[os.path.join("Minuit-1_7_9", "src", ".libs")],
+                             library_dirs=[os.path.join(minuitdir, "src", ".libs")],
                              libraries=["lcg_Minuit"],
-                             include_dirs=[os.path.join("Minuit-1_7_9")]
+                             include_dirs=[minuitdir]
                              )])
-
